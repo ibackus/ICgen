fixed precession calculations (units fix)

AddBinary.py
@@ -914,6 +914,49 @@ def trueToMean(nu, e):
     M = E - e * np.sin(E)
     return (M * RAD2DEG)
 
+#end function
+
+def calcMeanMotion(x1, x2, v1, v2, m1, m2, flag=True):
+    """
+    Given pynbody arrays for positions and velocity of primary and secondary bodies
+    and masses, calculates the Mean motion in the reduced two body system.
+    Usage note: Intended for binary system, but pass x2 = v2 = 0 to use with any
+    location in the disk.
+
+    Parameters
+    ----------
+    Assumed as pynbody SimArrays [preferred units]
+    
+    x1,x2: SimArrays
+        Primary and secondary position arrays [AU]
+    v1,v2: SimArrays
+        Primary and secondary velocity arrays [km/s]
+    m1, m2: SimArrays
+        Primary and secondary masses [Msol]
+    Flag: bool
+        Whether or not to internally convert to cgs units
+
+    Returns
+    -------
+    n: SimArray
+        Mean motion in 1/s
+    """
+    if flag:
+        #Ensure units are in cgs
+        x1 = x1.in_units('cm')
+        x2 = x2.in_units('cm')
+        v1 = v1.in_units('cm s**-1')
+        v2 = v2.in_units('cm s**-1')
+        m1 = m1.in_units('g')
+        m2 = m2.in_units('g')
+        
+    #Put binary in reduced mass frame, compute, return
+    a = SimArray(calcSemi(x1,x2,v1,v2,m1,m2,flag=False),'au').in_units('cm')
+    mu = G * (m1 + m2)
+    return np.sqrt(mu/np.power(a,3))
+    
+#end function
+
 ##########################################################################
 #                                                                                               #
 #	Functions for computing Cartesian coordinates from Keplerian orbital elements.              #
@@ -1123,7 +1166,7 @@ def initializeBinary(
 ##########################################################################
 
 
-def accretionEDot(Binary, Mdot, dt):
+def accretionEDot(Binary, M_dot, dt):
     """
     Given a Binary object and an accretion rate (assumed to be in M_sol/yr), compute the theoretical rate of change of the
     binary's eccentricity in 1/second.
@@ -1148,7 +1191,7 @@ def accretionEDot(Binary, Mdot, dt):
     e = Binary.e
     M = (Binary.m1 + Binary.m2) * Msol
     mu = BigG * M
-    Mdot *= Msol
+    Mdot = M_dot*Msol
     #dm = Mdot*dt
     r = a * (1.0 + e)
     eps = -mu / (2.0 * a)
@@ -1163,6 +1206,42 @@ def accretionEDot(Binary, Mdot, dt):
     return edot
 
 # end function
+
+def binaryPrecession(s,r_in,r_out):
+    """
+    Computes the period of the binary precession due to an axisymmetric disk (!!!)
+    given by Rafikov 2013.
+
+    Parameters
+    ----------
+    s : Tipsy snapshot
+    r_in, r_out : float
+        inner and outer radii of the circumbinary disk [AU]
+        
+    Returns
+    -------
+    T : SimArray
+        Period of binary argument of periastron precession in yr
+    """
+    x1 = s.stars[0]['pos'].in_units('cm')
+    x2 = s.stars[1]['pos'].in_units('cm')
+    v1 = s.stars[0]['vel'].in_units('cm s**-1')
+    v2 = s.stars[1]['vel'].in_units('cm s**-1')
+    m1 = s.stars[0]['mass'].in_units('g')
+    m2 = s.stars[1]['mass'].in_units('g')    
+    
+    # Define required parameters in cgs
+    M_bin = m1 + m2
+    M_disk = np.sum(s.gas['mass']).in_units('g')    
+    a = SimArray(calcSemi(x1,x2,v1,v2,m1,m2,flag=False),'au').in_units('cm') #semimajor axis in cm
+    n = calcMeanMotion(x1, x2, v1, v2, m1, m2, flag=False) #mean motion in 1/s    
+    r_in = SimArray(r_in,'au').in_units('cm')
+    r_out = SimArray(r_out,'au').in_units('cm')    
+    
+    T = 8.0*np.pi*(M_bin/M_disk)*(np.power(r_out,0.5)*np.power(r_in,2.5)/(np.power(a,3)*0.5*n))
+    return isaac.strip_units(T)/YEARSEC
+    
+#end function
 
 
 def calcCircularFrequency(x1, x2, v1, v2, m1, m2, flag=True):

binaryUtils.py
@@ -169,7 +169,7 @@ def find_crit_radius(r,array,toFind,num=1000):
 
 #end function
 
-def computeCOM(stars,gas,cutoff=None,starFlag=True):
+def computeCOM(stars,gas,cutoff=None,starFlag=True,gasFlag=True):
     """
     Given pynbody star and gas arrays, compute the center of mass for the entire specified system.
 
@@ -188,7 +188,9 @@ def computeCOM(stars,gas,cutoff=None,starFlag=True):
         Center of mass (in AU for each x,y,z component)
     """
     com = pynbody.array.SimArray(np.zeros(3),'au')	
- 
+
+    assert starFlag == True or gasFlag == True, "At least one flag must be true."    
+    
     #If there's a cutoff, select gas particles with cylindrical radius less than the cutoff
     if cutoff != None:
         mask = gas['rxy'] < cutoff
@@ -200,16 +202,25 @@ def computeCOM(stars,gas,cutoff=None,starFlag=True):
 	
         #Compute stellar mass, mass-weighted position
         starMass = stars[0]['mass'] + stars[1]['mass']
-        starPos = (stars[0]['pos']*stars[0]['mass'] + stars[1]['pos']*stars[1]['mass']).in_units('Msol au')
+        starPos = stars[0]['pos'].in_units('au')*stars[0]['mass']
+        starPos += stars[1]['pos'].in_units('au')*stars[1]['mass']
 	
-        #Compute, return total center of mass
-        com = (starPos + np.sum(gas['pos']*gas[0]['mass'],axis=0)).in_units('Msol au')
-        return (com/np.sum(starMass+np.sum(gas['mass']))).in_units('au')
+        if gasFlag:
+            #Compute, return total center of mass
+            com = starPos + np.sum(gas['pos'].in_units('au')*gas[0]['mass'],axis=0)
+            com /= (starMass + np.sum(gas['mass']))
+            return com
+        else:
+            com = (starPos/starMass)
+            return com
+          
     else: #No stars, just gas
-        com = np.sum(gas['pos'])*gas[0]['mass']/np.sum(gas['mass'])
-        return com.in_units('au')
+         com = np.sum(gas['pos'].in_units("au")*gas[0]['mass'],axis=0)
+         com /= np.sum(gas['mass'])
+         return com
 
-#end function
+#end function 
+ 
 
 def computeVelocityCOM(s,cutoff=None,starFlag=True,gasFlag=True):
     """
@@ -258,7 +269,7 @@ def computeVelocityCOM(s,cutoff=None,starFlag=True,gasFlag=True):
         if gasFlag:
             #Compute, return total center of mass
             com = starPos + np.sum(gas['vel'].in_units('km s**-1')*gas[0]['mass'],axis=0)
-            com /= starMass + np.sum(gas['mass'])
+            com /= (starMass + np.sum(gas['mass']))
             return com
         else:
             com = (starPos/starMass)
@@ -825,7 +836,7 @@ def orbElemsVsRadius(s,rBinEdges,average=False):
     
 #end function    
     
-def diskPrecession(s,r):
+def diskPrecession(s,radius):
     """
     Computes the precession of the disk due to the binary quadrupole moment.
     The precessions considered are kappa_r and kappa_z corresponding to the
@@ -840,7 +851,7 @@ def diskPrecession(s,r):
     
     s: Tipsy snapshot
     r: numpy array
-        array of radial bins centers in AU
+        array of radial bins centers [AU]
         
     Returns:
     -------
@@ -849,14 +860,16 @@ def diskPrecession(s,r):
         2 x len(rBinEdges)-1 array containing precession at each radial point in 1/s
     """
     #Compute relevant frequencies
-    alpha = 0.25
-    mu = ((s.stars[0]['mass'] * s.stars[1]['mass'])/np.sum(s.stars['mass']))*AddBinary.Msol
-    omega = np.sqrt((AddBinary.BigG*mu/np.power(r,3)) * (1.0 + 3.0*alpha/np.power(r,2)))
-    kappa_r = np.sqrt((AddBinary.BigG*mu/np.power(r,3)) * (1.0 - 3.0*alpha/np.power(r,2)))
-    kappa_z = np.sqrt((AddBinary.BigG*mu/np.power(r,3)) * (1.0 + 9.0*alpha/np.power(r,2)))
+    alpha = SimArray(0.25,'au**2')
+    r = SimArray(radius,'au')
+    grav = SimArray(4.0*np.pi**2,'au**3 yr**-2 Msol**-1')
+    mu = (s.stars[0]['mass'] * s.stars[1]['mass'])/np.sum(s.stars['mass'])
+    omega = np.sqrt((grav*mu/np.power(r,3)) * (1.0 + 3.0*alpha/np.power(r,2)))
+    kappa_r = np.sqrt((grav*mu/np.power(r,3)) * (1.0 - 3.0*alpha/np.power(r,2)))
+    kappa_z = np.sqrt((grav*mu/np.power(r,3)) * (1.0 + 9.0*alpha/np.power(r,2)))
     
     #Compute precession. > 0 -> preccesion, < 0 -> recession
-    omega_p = np.zeros((2,len(r)))
+    omega_p = SimArray(np.zeros((2,len(r))),'yr**-1')
     omega_p[0,:] = omega - kappa_r
     omega_p[1,:] = omega - kappa_z
     